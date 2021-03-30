a8cebd014ff9629da39b0820ac613063b794bf7a
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-03-30 21:21:18 +0600

    edited gomoku/views.py and requirements

--- gomoku/views.py
+++ gomoku/views.py
@@ -5,24 +5,28 @@ from .models import Party
 from .services import get_letters_from_a_to_o
 
 
-context_for_field = {
-    'count': range(15, 0, -1),
-    'count14': range(14),
-    'game': Game.objects.get(name='Гомоку'),
-    'letters': get_letters_from_a_to_o(),
-}
-
-
 def play_gomoku(request):
     """Главная страница Гомоку"""
 
-    return render(request, "gomoku/gomoku.html", context_for_field)
+    context = {
+        'count': range(15, 0, -1),
+        'count14': range(14),
+        'game': Game.objects.get(name='Гомоку'),
+        'letters': get_letters_from_a_to_o(),
+    }
+
+    return render(request, "gomoku/gomoku.html", context)
 
 
 def watch_party(request, id):
     """Посмотреть сыгранную партию"""
 
-    context = context_for_field
-    context['party'] = Party.objects.get(id=id)
+    context = {
+        'count': range(15, 0, -1),
+        'count14': range(14),
+        'game': Game.objects.get(name='Гомоку'),
+        'letters': get_letters_from_a_to_o(),
+        'party': Party.objects.get(id=id),
+    }
 
     return render(request, 'gomoku/watch_party.html', context)
