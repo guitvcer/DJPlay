f34f5bb9c6143a1e57886595b086865c573685e1
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-05-26 13:59:31 +0600

    edited setup.py

--- account/management/commands/setup.py
+++ account/management/commands/setup.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
 from account.models import Game, Queue
 
 
@@ -8,16 +9,25 @@ class Command(BaseCommand):
     help = "Setting up database"
 
     def handle(self, *args, **options):
-        gomoku = Game.objects.create(
-            name="Гомоку",
-            app_name="gomoku",
-            rules="Гомоку — настольная логическая игра для двух игроков."
-                  " На квадратной доске размером 15×15 пунктов игроки поочерёдно выставляют камни двух цветов."
-                  " Выигрывает тот, кто первым построит непрерывный ряд из пяти камней своего цвета по вертикали,"
-                  " горизонтали или диагонали.",
-            image="/gomoku.png",
-            is_released=True,
-        )
-        Queue.objects.create(game=gomoku)
+        try:
+            try:
+                gomoku = Game.objects.get(name="Гомоку")
+                Queue.objects.get(game=gomoku)
 
-        self.stdout.write(self.style.SUCCESS('Database was successfully set up'))
+                self.stdout.write(self.style.SUCCESS('Database was successfully set up'))
+            except (Game.DoesNotExist, Queue.DoesNotExist):
+                gomoku = Game.objects.create(
+                    name="Гомоку",
+                    app_name="gomoku",
+                    rules="Гомоку — настольная логическая игра для двух игроков."
+                          " На квадратной доске размером 15×15 пунктов игроки поочерёдно выставляют камни двух цветов."
+                          " Выигрывает тот, кто первым построит непрерывный ряд из пяти камней своего цвета по вертикали,"
+                          " горизонтали или диагонали.",
+                    image="/gomoku.png",
+                    is_released=True,
+                )
+                Queue.objects.create(game=gomoku)
+
+                self.stdout.write(self.style.SUCCESS('Database was successfully set up'))
+        except OperationalError:
+            self.stdout.write(self.style.ERROR('Apply migrations by "python manage.py migrate"'))
