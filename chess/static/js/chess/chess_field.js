af2d7231b223a9d8bb5bce941f2f6a3efe6fa078
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-06-05 21:08:39 +0600

    refactored chess_field.js

--- chess/static/js/chess/chess_field.js
+++ chess/static/js/chess/chess_field.js
@@ -1,6 +1,6 @@
 const field = document.querySelector('.field'), a_to_h = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-let selected_piece;
+let selected_piece, color = 'white';
 
 
 // заполнить поле
@@ -65,49 +65,252 @@ function unselectAllCells() {
     for (let cell of document.querySelectorAll('.fieldCell')) {
         cell.classList.remove('selected');
     }
+    for (let i of document.querySelectorAll('.available-move')) {
+        i.classList.remove('available-move');
+    }
+
+    selectedPiece = null;
 }
 
+
+// выделить доступные клетки для ладьи
 function selectForRook(letter, number) {
-    for (let i of a_to_h) {
-        document.querySelector(`#${i}${number}`).classList.add('available-move');
-    }
-    for (let i = 1; i <= 8; i++) {
-        document.querySelector(`#${letter}${i}`).classList.add('available-move');
-    }
+    // вверх по вертикали
+    for (let i = number + 1; i <= 8; i++)
+        if (!addAvailableMove(`${letter}${i}`)) break;
+
+    // вниз по вертикали
+    for (let i = number - 1; i >= 1; i--)
+        if (!addAvailableMove(`${letter}${i}`)) break;
+
+
+    let indexOfLetter = a_to_h.indexOf(letter);
+
+    // влево по горизонтали
+    for (let i = indexOfLetter - 1; i >= 0; i--)
+        if (!addAvailableMove(`${a_to_h[i]}${number}`)) break;
+
+    // вправо по горизонтали
+    for (let i = indexOfLetter + 1; i <= 7; i++)
+        if (!addAvailableMove(`${a_to_h[i]}${number}`)) break;
 }
 
+
+// выделить клетки для слона
 function selectForBishop(indexOfLetter, number) {
     for (let i = 1; i <= 8; i++) {
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + i]}${number + i}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + i]}${number - i}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - i]}${number - i}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - i]}${number + i}`)
-                .classList.add('available-move');
-        } catch (e) {}
+        if (!addAvailableMove(`${a_to_h[indexOfLetter + i]}${number + i}`)) break;
     }
+
+    for (let i = 1; i <= 8; i++) {
+        if (!addAvailableMove(`${a_to_h[indexOfLetter + i]}${number - i}`)) break;
+    }
+
+    for (let i = 1; i <= 8; i++) {
+        if (!addAvailableMove(`${a_to_h[indexOfLetter - i]}${number + i}`)) break;
+    }
+
+    for (let i = 1; i <= 8; i++) {
+        if (!addAvailableMove(`${a_to_h[indexOfLetter - i]}${number - i}`)) break;
+    }
+}
+
+
+// выделить клетки для пешки
+function selectForPawn(colorOfPiece, letter, number) {
+    let el = document.querySelector(`#${letter}${number}`);
+
+    if (colorOfPiece === 'white') {
+        let indexOfLetter = a_to_h.indexOf(letter),
+            rightFrontCell = document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number + 1}`),
+            leftFrontCell = document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number + 1}`),
+            frontCell = document.querySelector(`#${letter}${number + 1}`);
+
+        if (!hasElementEnemyPiece(frontCell)) {
+            addAvailableMove(frontCell.id);
+        }
+
+        if (hasElementEnemyPiece(rightFrontCell)) {
+            addAvailableMove(rightFrontCell.id);
+
+            if (hasElementEnemyPiece(leftFrontCell)) {
+                addAvailableMove(leftFrontCell.id);
+            }
+        } else if (hasElementEnemyPiece(leftFrontCell)) {
+            addAvailableMove(leftFrontCell.id);
+        }
+
+        if (!el.firstChild.classList.contains('moved'))
+            addAvailableMove(`${letter}${number + 2}`);
+    } else {
+        let indexOfLetter = a_to_h.indexOf(letter),
+            rightFrontCell = document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number - 1}`),
+            leftFrontCell = document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number - 1}`),
+            frontCell = document.querySelector(`#${letter}${number - 1}`);
+
+        if (!hasElementEnemyPiece(frontCell)) {
+            addAvailableMove(frontCell.id);
+        }
+
+        if (hasElementEnemyPiece(rightFrontCell)) {
+            addAvailableMove(rightFrontCell.id);
+
+            if (hasElementEnemyPiece(leftFrontCell)) {
+                addAvailableMove(leftFrontCell.id);
+            }
+        } else if (hasElementEnemyPiece(leftFrontCell)) {
+            addAvailableMove(leftFrontCell.id);
+        }
+
+        if (!el.firstChild.classList.contains('moved'))
+            addAvailableMove(`${letter}${number - 2}`);
+    }
+}
+
+
+// выделить клетки для коня
+function selectForKnight(indexOfLetter, number) {
+    addAvailableMove(`${a_to_h[indexOfLetter - 1]}${number + 2}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 1]}${number + 2}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 1]}${number - 2}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 1]}${number - 2}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 2]}${number + 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 2]}${number - 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 2]}${number + 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 2]}${number - 1}`);
+}
+
+
+// выделить клетки для ферзя
+function selectForQueen(indexOfLetter, letter, number) {
+    selectForRook(letter, number);
+    selectForBishop(indexOfLetter, number);
+}
+
+
+// выделить клетки для короля
+function selectForKing(indexOfLetter, number) {
+    addAvailableMove(`${a_to_h[indexOfLetter]}${number + 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 1]}${number + 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 1]}${number}`);
+    addAvailableMove(`${a_to_h[indexOfLetter + 1]}${number - 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter]}${number - 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 1]}${number - 1}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 1]}${number}`);
+    addAvailableMove(`${a_to_h[indexOfLetter - 1]}${number + 1}`);
+}
+
+
+// выбрать клетки для фигуры
+function selectForPiece(selectedCell) {
+    let letter = selectedCell.id.split("")[0],
+        number = Number(selectedCell.id.split("")[1]),
+        piece = selectedCell.firstChild.classList[1],
+        colorOfPiece = selectedCell.firstChild.classList[2],
+        indexOfLetter = a_to_h.indexOf(letter);
+
+    if (piece === 'pawn') {
+        selectForPawn(colorOfPiece, letter, number);
+    } else if (piece === 'rook') {
+        selectForRook(letter, number);
+    } else if (piece === 'knight') {
+        selectForKnight(indexOfLetter, number);
+    } else if (piece === 'bishop') {
+        selectForBishop(indexOfLetter, number);
+    } else if (piece === 'queen') {
+        selectForQueen(indexOfLetter, letter, number);
+    } else if (piece === 'king') {
+        selectForKing(indexOfLetter, number);
+    }
+}
+
+
+// получить наружный div фигуры (img)
+function getDivOfPiece(el) {
+    if (el.classList.contains('fieldCell')) return el;
+
+    if (el.closest('.fieldCell')) return el.closest('.fieldCell');
+
+    return false;
+}
+
+
+// имеет ли элемент дружную фигуру
+function hasElementFriendPiece(el) {
+    if (el.firstChild == null) return false;
+
+    return el.firstChild.classList.contains(color);
+}
+
+// имеет ли элемент враждебную фигуру
+function hasElementEnemyPiece(el) {
+    if (el == null || el.firstChild == null) return false;
+
+    return !el.firstChild.classList.contains(color);
+}
+
+
+// доступна ли клетка для фигуры
+function isCellAvailableForPiece(id) {
+    let el = document.querySelector(`#${id}`);
+
+    if (hasElementEnemyPiece(el)) return 'enemy_piece';
+
+    if (hasElementFriendPiece(el)) return 'friend_piece';
+
+    return 'empty_cell';
+}
+
+
+// Добавить .availableMove выбранному элементу
+function addAvailableMove(id) {
+    el = document.getElementById(id);
+
+    if (el == null) return;
+
+    if (isCellAvailableForPiece(id) === 'empty_cell') {
+        el.classList.add('available-move');
+        return true;
+    } else if (isCellAvailableForPiece(id) === 'enemy_piece') {
+        el.classList.add('available-move');
+        return false;
+    }
+    else return false;
+}
+
+// передвинуть фигуру
+function movePiece(el) {
+    el = getDivOfPiece(el);
+
+    if (!hasElementFriendPiece(el) && el.classList.contains('available-move')) {
+        el.innerHTML = selectedPiece.innerHTML;
+        selectedPiece.innerHTML = "";
+        selectedPiece = null;
+        el.firstChild.classList.add('moved');
+
+        unselectAllCells();
+
+        return true;
+    }
+
+    return false;
 }
 
 
 // обработчик для поля
 document.body.addEventListener('click', function(event) {
-    for (let i of document.querySelectorAll('.available-move')) {
-        i.classList.remove('available-move');
-    }
+    if (!(event.target.classList.contains('field') || event.target.closest('.field')))
+        return;
+
+    // передвинуть фигуру или съесть
+    if (movePiece(event.target)) return;
+
+    unselectAllCells();
 
     // если выбранная клетка уже выбрана, то отменить выбор и выйти из функции
+
+    el = getDivOfPiece(event.target);
+
     if (event.target.tagName === 'IMG') {
         if (event.target.closest('.fieldCell').classList.contains('selected')) {
             unselectAllCells();
@@ -126,117 +329,24 @@ document.body.addEventListener('click', function(event) {
     let selectedCell;
 
     // выбрать нажатую клетку
-    if (event.target.tagName === 'IMG') {
+    if (event.target.tagName === 'IMG' && event.target.classList.contains(color)) {
         selectedCell = event.target.closest('.fieldCell');
     } else if (event.target.classList.contains('fieldCell')) {
         try {
-            if (event.target.firstChild.tagName === 'IMG') {
+            if (event.target.firstChild.tagName === 'IMG' && event.target.firstChild.classList.contains(color)) {
                 selectedCell = event.target;
             }
         } catch(e) {
             return;
         }
-    }
+    } else return;
 
     selectedPiece = selectedCell;
 
     selectedCell.classList.add('selected');
 
-    let letter = selectedCell.id.split("")[0],
-        number = Number(selectedCell.id.split("")[1]),
-        piece = selectedCell.firstChild.classList[1],
-        color = selectedCell.firstChild.classList[2],
-        indexOfLetter = a_to_h.indexOf(letter);
-
-    if (piece === 'pawn') {
-        // выбрать доступные ходы для белой пешки
-        if (color === 'white') {
-            document.querySelector(`#${letter}${number+1}`).classList.add('available-move');
-            document.querySelector(`#${letter}${number+2}`).classList.add('available-move');
-        } else {
-            document.querySelector(`#${letter}${number-1}`).classList.add('available-move');
-            document.querySelector(`#${letter}${number-2}`).classList.add('available-move');
-        }
-    } else if (piece === 'rook') {
-        selectForRook(letter, number);
-    } else if (piece === 'knight') {
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number + 2}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number + 2}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number - 2}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number - 2}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 2]}${number + 1}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 2]}${number - 1}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 2]}${number + 1}`)
-                .classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 2]}${number - 1}`)
-                .classList.add('available-move');
-        } catch (e) {}
-    } else if (piece === 'bishop') {
-        selectForBishop(indexOfLetter, number);
-    } else if (piece === 'queen') {
-        selectForRook(letter, number);
-        selectForBishop(indexOfLetter, number);
-    } else if (piece === 'king') {
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter]}${number + 1}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number + 1}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter + 1]}${number - 1}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter]}${number - 1}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number - 1}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number}`).classList.add('available-move');
-        } catch (e) {}
-
-        try {
-            document.querySelector(`#${a_to_h[indexOfLetter - 1]}${number + 1}`).classList.add('available-move');
-        } catch (e) {}
-    }
+    // выделить клетки для фигуры
+    selectForPiece(selectedCell);
 });
 
 // обработчик escape
@@ -244,12 +354,9 @@ document.body.addEventListener('keydown', function(event) {
     if (event.keyCode === 27) {
         unselectAllCells();
     }
-    for (let i of document.querySelectorAll('.available-move')) {
-        i.classList.remove('available-move');
-    }
 })
 
 
-fillField('white');
+fillField(color);
 resizeField();
 window.addEventListener('resize', resizeField);
