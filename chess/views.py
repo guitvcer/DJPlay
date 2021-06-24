a9ed49d55ad2ee04205e562214646d63ebe46779
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-06-24 19:46:25 +0600

    added returning http403 for unreleased game

--- chess/views.py
+++ chess/views.py
@@ -1,7 +1,16 @@
+from django.conf import settings
+from django.http import HttpResponseForbidden
 from django.views.generic import TemplateView
+from account.models import Game
 
 
 class PlayChessView(TemplateView):
     """Главная страница Шахмат"""
 
     template_name = 'chess/chess.html'
+
+    def get(self, request, *args, **kwargs):
+        if not Game.objects.get(name='Шахматы').is_released and not settings.DEBUG:
+            return HttpResponseForbidden()
+
+        return super().get(request, *args, **kwargs)
--- gomoku/views.py
+++ gomoku/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.http import HttpResponseForbidden
 from django.views.generic import TemplateView, DetailView
-
+from account.models import Game
 from .models import Party
 
 
@@ -8,6 +10,12 @@ class PlayGomokuView(TemplateView):
 
     template_name = 'gomoku/gomoku.html'
 
+    def get(self, request, *args, **kwargs):
+        if not Game.objects.get(name='Гомоку').is_released and not settings.DEBUG:
+            return HttpResponseForbidden()
+
+        return super().get(request, *args, **kwargs)
+
 
 class WatchPartyView(DetailView):
     """Посмотреть сыгранную партию"""
@@ -15,6 +23,12 @@ class WatchPartyView(DetailView):
     model = Party
     template_name = 'gomoku/watch_party.html'
     context_object_name = 'party'
+
+    def get(self, request, *args, **kwargs):
+        if not Game.objects.get(name='Гомоку').is_released and not settings.DEBUG:
+            return HttpResponseForbidden()
+
+        return super().get(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
         return Party.objects.get(id=self.kwargs['id'])
