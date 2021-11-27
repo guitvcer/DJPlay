0a624f7edbfecb9d5eba72d136a4ea9d07fc02db
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-11-27 11:19:45 +0600

    fixed bugs and refactored code of error pages

--- frontend/src/components/ErrorPages/Forbidden.vue
+++ frontend/src/components/ErrorPages/Forbidden.vue
@@ -2,7 +2,13 @@
   <section class="flex flex-col items-center pt-32">
     <h2 class="font-bold text-9xl">403</h2>
     <p class="font-bold text-3xl mt-6 mb-3">Страница не доступна</p>
-    <router-link to="/" class="font-semibold" @click="updateStatus(200)">На главную</router-link> </section>
+
+    <router-link
+      to="/"
+      class="font-semibold"
+      @click="updateStatus(200)"
+    >На главную</router-link>
+  </section>
 </template>
 
 <script>
--- frontend/src/components/ErrorPages/ServerError.vue
+++ frontend/src/components/ErrorPages/ServerError.vue
@@ -2,5 +2,19 @@
   <section class="flex flex-col items-center pt-32">
     <h2 class="font-bold text-9xl">500</h2>
     <p class="font-bold text-3xl mt-6 mb-3">Внутренняя ошибка сервера</p>
-    <router-link to="/" class="font-semibold" @click="$emit('redirect')">На главную</router-link> </section>
+
+    <router-link
+      to="/"
+      class="font-semibold"
+      @click="updateStatus(200)"
+    >На главную</router-link>
+  </section>
 </template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+  methods: mapMutations(["updateStatus"]),
+}
+</script>
