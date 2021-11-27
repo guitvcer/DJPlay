656bf0f80f1e87c2a136414637f12e1c6cb97a9a
guitvcer <abdrakhman.galaxy.s2@gmail.com>
2021-11-27 11:55:50 +0600

    size of tab is 2 spaces in router.js, added extensions in imports of components in router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,115 +2,115 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/account/Home.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: Home,
-    },
-    {
-        path: "/gomoku/",
-        component: () => import("./views/gomoku/Gomoku"),
-        name: "gomoku",
-    },
-    {
-        path: "/gomoku/:id/",
-        component: () => import("./views/gomoku/GomokuParty"),
-        name: "gomokuParty",
-    },
-    {
-        path: "/chess/",
-        component: () => import("./views/chess/Chess"),
-        name: "chess",
-    },
-    {
-        path: "/account/",
-        component: () => import("./views/account/Profile"),
-        name: "userProfile",
-    },
-    {
-        path: "/account/friends/",
-        component: () => import("./views/account/UsersList"),
-        name: "friends",
-    },
-    {
-        path: "/account/views/",
-        component: () => import("./views/account/UsersList"),
-        name: "viewers",
-    },
-    {
-        path: "/account/:username/",
-        name: "profile",
-        component: () => import("./views/account/Profile"),
-    },
-    {
-        path: "/account/:username/views/",
-        name: "usersViewers",
-        component: () => import("./views/account/UsersList"),
-    },
-    {
-        path: "/account/:username/friends/",
-        name: "usersFriends",
-        component: () => import("./views/account/UsersList"),
-    },
-    {
-        path: "/account/:username/friend-request",
-        name: "friendRequest",
-        redirect: "/:username/",
-    },
-    {
-        path: "/account/:username/chat",
-        name: "userChat",
-        redirect: "/:username/",
-    },
-    {
-        path: "/account/:username/party-list/",
-        name: "userPartyList",
-        component: () => import("./views/account/UserPartyList"),
-    },
-    {
-        path: "/account/users/",
-        name: "users",
-        component: () => import("./views/account/UsersList"),
-    },
-    {
-        path: "/account/edit/",
-        name: "editProfile",
-        component: () => import("./views/account/EditProfile"),
-    },
-    {
-        path: "/account/party-list/",
-        name: "partyList",
-        component: () => import("./views/account/UserPartyList"),
-    },
-    {
-        path: "/account/google-oauth2/",
-        name: "googleOAuth2",
-        component: () => import("./views/account/GoogleOAuth2"),
-    },
-    {
-        path: "/account/vk-oauth2/",
-        name: "vkOAuth2",
-        component: () => import("./views/account/VKOAuth2"),
-    },
-    {
-        path: "/chat/",
-        name: "chats",
-        component: () => import("./views/chat/Chat"),
-    },
-    {
-        path: "/chat/:username/",
-        name: "chat",
-        component: () => import("./views/chat/Chat"),
-    },
-    {
-        path: "/:catchAll(.*)",
-        component: () => import("./components/ErrorPages/NotFound"),
-    },
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/gomoku/",
+    component: () => import("./views/gomoku/Gomoku.vue"),
+    name: "gomoku",
+  },
+  {
+    path: "/gomoku/:id/",
+    component: () => import("./views/gomoku/GomokuParty.vue"),
+    name: "gomokuParty",
+  },
+  {
+    path: "/chess/",
+    component: () => import("./views/chess/Chess.vue"),
+    name: "chess",
+  },
+  {
+    path: "/account/",
+    component: () => import("./views/account/Profile.vue"),
+    name: "userProfile",
+  },
+  {
+    path: "/account/friends/",
+    component: () => import("./views/account/UsersList.vue"),
+    name: "friends",
+  },
+  {
+    path: "/account/views/",
+    component: () => import("./views/account/UsersList.vue"),
+    name: "viewers",
+  },
+  {
+    path: "/account/:username/",
+    name: "profile",
+    component: () => import("./views/account/Profile.vue"),
+  },
+  {
+    path: "/account/:username/views/",
+    name: "usersViewers",
+    component: () => import("./views/account/UsersList.vue"),
+  },
+  {
+    path: "/account/:username/friends/",
+    name: "usersFriends",
+    component: () => import("./views/account/UsersList.vue"),
+  },
+  {
+    path: "/account/:username/friend-request",
+    name: "friendRequest",
+    redirect: "/:username/",
+  },
+  {
+    path: "/account/:username/chat",
+    name: "userChat",
+    redirect: "/:username/",
+  },
+  {
+    path: "/account/:username/party-list/",
+    name: "userPartyList",
+    component: () => import("./views/account/UserPartyList.vue"),
+  },
+  {
+    path: "/account/users/",
+    name: "users",
+    component: () => import("./views/account/UsersList.vue"),
+  },
+  {
+    path: "/account/edit/",
+    name: "editProfile",
+    component: () => import("./views/account/EditProfile.vue"),
+  },
+  {
+    path: "/account/party-list/",
+    name: "partyList",
+    component: () => import("./views/account/UserPartyList.vue"),
+  },
+  {
+    path: "/account/google-oauth2/",
+    name: "googleOAuth2",
+    component: () => import("./views/account/GoogleOAuth2.vue"),
+  },
+  {
+    path: "/account/vk-oauth2/",
+    name: "vkOAuth2",
+    component: () => import("./views/account/VKOAuth2.vue"),
+  },
+  {
+    path: "/chat/",
+    name: "chats",
+    component: () => import("./views/chat/Chat.vue"),
+  },
+  {
+    path: "/chat/:username/",
+    name: "chat",
+    component: () => import("./views/chat/Chat.vue"),
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: () => import("./components/ErrorPages/NotFound.vue"),
+  },
 ];
 
 const router = new createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
